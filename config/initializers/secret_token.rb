43b03eb395c14835ff97982ed6fd5f042e23d23a
Peak Xu <pxu@carbonite.com>
2012-06-09 14:39:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9cc577d10078ada2e338025e5fb7584cc387be154515e03ffd36cc94f690d0e071739ab388355402514cdc73cb11b6b9723aadd860e8cb256045ea6c808b0898'
